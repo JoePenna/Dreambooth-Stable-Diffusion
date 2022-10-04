01b42a4d6eae588196ee593730dbd1e6d07a070a
GpuLover <114974844+GpuLover@users.noreply.github.com>
2022-10-04 07:32:31 +0200

    add --store_prompt_conf flag to stable_txt2img
    you won't forget wich seed or scale you used,
    stores a json with all parameters used along the grid image

--- scripts/stable_txt2img.py
+++ scripts/stable_txt2img.py
@@ -1,4 +1,5 @@
 import argparse, os, sys, glob
+import json
 import torch
 import numpy as np
 from omegaconf import OmegaConf
@@ -179,7 +180,13 @@ def main():
     parser.add_argument(
         "--embedding_path", 
         type=str, 
-        help="Path to a pre-trained embedding manager checkpoint")
+        help="Path to a pre-trained embedding manager checkpoint"
+    )
+    parser.add_argument(
+        "--store_prompt_conf",
+        action='store_true',
+        help="write a json file with all the parameters/flags used when running a prompt",
+    )
 
     opt = parser.parse_args()
 
@@ -277,10 +284,13 @@ def main():
 
                     # to image
                     grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                    Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'{prompt.replace(" ", "-")}-{grid_count:04}.jpg'))
+                    grid_filename = os.path.join(outpath, f'{prompt.replace(" ", "-")}-{grid_count:04}.jpg')
+                    Image.fromarray(grid.astype(np.uint8)).save(grid_filename)
                     grid_count += 1
                     
-                    
+                    if opt.store_prompt_conf:
+                        with open(os.path.splitext(grid_filename)[0] + '.json', "w") as f:
+                            f.write(json.dumps(opt.__dict__, indent=4))
 
                 toc = time.time()
 
