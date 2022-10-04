fc1427c3e0a53a8ac13ef0c2050e705a3333e47c
Fabio 'MrWHO' Torchetti <fabbari@gmail.com>
2022-10-04 00:56:14 +0000

    Move pruning to own module

---
+++ ldm/pruner.py
@@ -0,0 +1,17 @@
+def prune_checkpoint(old_state):
+    print(f"Prunin' Checkpoint")
+    pruned_checkpoint = dict()
+    print(f"Checkpoint Keys: {old_state.keys()}")
+    for key in old_state.keys():
+        if key != "optimizer_states":
+            pruned_checkpoint[key] = old_state[key]
+    else:
+        print("Removing optimizer states from checkpoint")
+    if "global_step" in old_state:
+        print(f"This is global step {old_state['global_step']}.")
+    old_state = pruned_checkpoint['state_dict'].copy()
+    new_state = dict()
+    for key in old_state:
+        new_state[key] = old_state[key].half()
+    pruned_checkpoint['state_dict'] = new_state
+    return pruned_checkpoint
