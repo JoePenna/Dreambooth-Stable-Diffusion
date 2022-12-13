64ffc4207f922ee4f2fb569cd53b47d35ff3d3c8
Tommy Mönnich <tommy.moennich@gmx.de>
2022-12-13 16:22:19 +0100

    Remove unused import

--- scripts/stable_txt2img.py
+++ scripts/stable_txt2img.py
@@ -15,7 +15,6 @@ from contextlib import contextmanager, nullcontext
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
-from ldm.models.diffusion.dpm_solver import DPMSolverSampler
 
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
