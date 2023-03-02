be5eb1120c2248cc62ffc0a7b67d742703eb6f9d
David Bielejeski <2463282+djbielejeski@users.noreply.github.com>
2023-03-02 15:29:56 -0600

    Cleaner faster impl.

--- ldm/modules/diffusionmodules/util.py
+++ ldm/modules/diffusionmodules/util.py
@@ -266,8 +266,7 @@ def noise_like(shape, device, repeat=False, add_offset_noise=True):
     repeat_noise = lambda: torch.randn((1, *shape[1:]), device=device).repeat(shape[0], *((1,) * (len(shape) - 1)))
 
     # Using info from https://www.crosslabs.org//blog/diffusion-with-offset-noise
-    offset_noise = 0.1 * torch.randn(shape[0], shape[1], 1, 1, device=device) if add_offset_noise else 0
+    offset_noise = lambda: 0.1 * torch.randn(shape[0], shape[1], 1, 1, device=device) if add_offset_noise else 0
+    noise = lambda: torch.randn(shape, device=device)
 
-    noise = lambda: torch.randn(shape, device=device) + offset_noise
-
-    return repeat_noise() if repeat else noise()
+    return repeat_noise() if repeat else noise() + offset_noise()
